f98799f773f0767271d97d7ec5540d5c9c860d1b
Ben Clifford <bobsbeenjamin@netscape.net>
2014-06-03 01:00:30 -0700

    Changed board encoding from array of ints to array of chars

--- Pregame/4-in-a-line_PregameComputer.py
+++ Pregame/4-in-a-line_PregameComputer.py
@@ -56,13 +56,13 @@ def precompute(depth):
     @return Array with all computed boards, stored as Board objects"""
     # NOTE to Jovanni: This is not working yet. The function documentation shows what it 
     # should do.
-    boards = createAllBoardsAtSingleDepth(int(depth), False, 1)
-    logMe( "Number of boards created: " + str(len(boards)) )
-##    return # comment this out to see all of the boards
-    for board in boards:
-        logMe(board)
-        return
-        displayBoard(board)
+    try:
+        boards = createAllBoardsAtSingleDepth(int(depth), False, 1)
+    finally:
+        logMe( "Number of boards created: " + str(len(boards)) )
+        return # comment this out to see all of the boards
+        for board in boards:
+            displayBoard(board)
     
 
 def createAllBoardsAtSingleDepth(depth, partialBoard, x_or_o):
@@ -70,7 +70,7 @@ def createAllBoardsAtSingleDepth(depth, partialBoard, x_or_o):
     called outside of itself, partialBoard should be False, and x_or_o should be 1.
     @return Array with all created boards, stored as arrays of -1's , 0's and 1's"""
     boards = [] # this holds all boards that will actually be returned by this function
-    x_or_o = 1
+    x_or_o = "X"
     n = 0 # this is unnecessary, but makes the code more readable
     while n < depth:
         # Holds all valid boards with (depth-n) pieces; clear array for each n
@@ -78,14 +78,14 @@ def createAllBoardsAtSingleDepth(depth, partialBoard, x_or_o):
         # Base case: for each spot on the board, create a unique board with an X 
         if n==0:
             for idx in range(64):
-                newBoard = [0]*64
+                newBoard = ["-"]*64
                 newBoard[idx] = x_or_o
                 boardsAtThisSubLevel.append(newBoard)
         # The 'recursive' part of this iterative function
         else:
             for prevBoard in boardsAtPrevSubLevel:
                 for idx in range(64):
-                    if not prevBoard[idx]: # only place piece on empty spot
+                    if prevBoard[idx]=="-": # only place a piece on an empty spot
                         newBoard = prevBoard[:] # local copy
                         newBoard[idx] = x_or_o
                         boardsAtThisSubLevel.append(newBoard)
@@ -94,7 +94,7 @@ def createAllBoardsAtSingleDepth(depth, partialBoard, x_or_o):
         # This effectively copies the current board set to a new variable (quick style)
         boardsAtPrevSubLevel = boardsAtThisSubLevel
         # Update move value (x_or_o) and sublevel (n)
-        x_or_o *= -1
+        x_or_o = "X" if x_or_o=="O" else "O"
         n += 1
     return boardsAtThisSubLevel
     
@@ -118,26 +118,9 @@ def displayBoard(board):
     logMe("  1 2 3 4 5 6 7 8")
     boardStr = ""
     for piece in board:
-        if piece==1:
-            boardStr += " X"
-        if piece==-1:
-            boardStr += " O"
-        if piece==0:
-            boardStr += " -"
+        boardStr += " " + piece
     for row in range(0, 128, 16):
         logMe(chr(row/8+65) + boardStr[row:row+16])
-    # NOTE to Jovanni: The commented code below is an alternate way to display the board 
-    # that works well too. I was testing both to see which performs faster (this is useful
-    # for testing). The 2 algorithms perform about the same, so I kept the easier read.
-##    for row in range(8):
-##        # Start with one of the characters A-H
-##        rowStr = chr(row+65) + " "
-##        # Set up string builder
-##        row *= 8
-##        row = board[row:row+8]
-##        # Build each row via list comprehension and display it
-##        rowStr += " ".join(["X" if cell==1 else "O" if cell==-1 else "-" for cell in row])
-##        logMe(rowStr)
     
 
 def logMe(strToLog="", runSilent=False):
