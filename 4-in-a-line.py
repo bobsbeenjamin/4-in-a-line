38a8acbe3bd1d9280737566d7caacdd921b10596
Jovanni Cutigni <thejacx@gmail.com>
2014-06-05 14:52:36 -0700

    import evalBoard from module

--- 4-in-a-line.py
+++ 4-in-a-line.py
@@ -8,6 +8,7 @@
 
 # Imports #
 import multiprocessing, random, time
+from evalboard import evalBoard
 # Globals #
 logStr = "Welcome to the 4-in-a-line Program!"
 
@@ -157,81 +158,6 @@ def getBestChildren(parent, x_or_o):
     return children
     
 
-def evalBoard(board):
-    """Utility function that takes a board and returns an integer rating of
-    the desirability of that state of that board"""
-    sum = 0
-    # scan every column
-    for colStart in xrange(0, 8, 1):
-        sum += evalLine(board, xrange(colStart, 64, 8))
-    # scan every row
-    for rowStart in xrange(0, 64, 8):
-        sum += evalLine(board, xrange(rowStart, rowStart + 8, 1))
-    return sum
-    
-
-def evalLine(board, line):
-    """Utility value of a single board line"""
-    signFor = lambda c: 1 if c == 'X' else -1 if c == 'O' else 0
-    def valueOf(run, count, space):
-        if run >= 4:
-            # won.
-            return 16384
-        elif count + space < 4:
-            # a non-winning run with no room to expand is worthless
-            return 0
-        elif count > 0:
-            # straight run is best, nearby pieces seperated by spaces good,
-            # room to expand is desirable
-            bonus = count - run
-            return (4 ** run) + (2 ** bonus if bonus > 0 else 0) + space
-        else:
-            return 0
-
-    total = 0 # sum
-
-    space = 0 # spaces on the ends or within this stretch
-    carry = 0 # straight space that carries over
-    count = 0 # count of symbols in this stretch (non-contigious)
-    run = 0   # count of contigioius symbols
-    maxrun = 0
-    last = '' # last player symbol encountered
-
-    for i in line:
-        c = board[i]
-
-        # total and switch if we encounter a different player symbol
-        if c != last and c != '-':
-            # switch symbols, end run
-            total += valueOf(maxrun, count, space) * signFor(last)
-            space, count, run, maxrun = carry, 0, 0, 0
-            last = c
-
-        # count it
-        if c == '-':
-            space += 1
-            carry += 1
-            run = 0 # run broken by space
-        else:
-            if carry > 2 and len(last) > 0:
-                # too much space between same symbol, end run
-                total += valueOf(maxrun, count, space) * signFor(last)
-                space, count, run, maxrun = carry, 0, 0, 0
-                # don't reset last
-            run += 1
-            maxrun = max(run, maxrun)
-            count += 1
-            carry = 0 # space run broken
-
-    #finished scanning line
-    if (count > 0):
-        # have leftovers, end run
-        total += valueOf(maxrun, count, space) * signFor(last)
-        space, count, run, maxrun = carry, 0, 0, 0
-        last = ''
-
-    return total
-    
 
 def getBestBoardPlusMove(leaf):
     """Chains back from leaf to root, then keeps the direct child of root that led to 
